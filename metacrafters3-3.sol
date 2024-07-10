16cd9cc0fecc4bf7be17c7449e3224b7629eda2c
SAI JAGAN KOTNI <123167013+jagan0708@users.noreply.github.com>
2024-07-10 19:00:38 +0530

    Update metacrafters3-3.sol

--- metacrafters3-3.sol
+++ metacrafters3-3.sol
@@ -1,11 +1,16 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
 
-contract Jagan {
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Jagan is ERC20
+ {
     address public owner;
     string public tokenName;
     string public tokenSymbol;
-    uint256 public totalSupply;
+    
 
     mapping(address => uint256) public balances;
 
@@ -14,23 +19,23 @@ contract Jagan {
         _;
     }
 
-    constructor() {
+    constructor() ERC20("sai","vs") {
         owner = msg.sender;
-        tokenName = "Jagan";
+        tokenName = "Sai";
         tokenSymbol = "Vs";
-        totalSupply = 0;
+        
     }
 
    function mint(address _address, uint256 _value) public onlyOwner{
         require(_address == owner, "Only the contract owner can mint tokens");
-        totalSupply += _value; 
+        
         balances[_address] += _value;
     }
 
 
    function burn(address _address, uint256 _value) public {
         if (balances[_address] >= _value) {
-            totalSupply -= _value; 
+            
             balances[_address] -= _value;
         }
     }
